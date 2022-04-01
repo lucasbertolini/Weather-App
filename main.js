5dbf335c39198e26aea263d87407a43905d5db33
Lucas Bertolini <42697227+lucasbertolini@users.noreply.github.com>
2022-03-31 22:31:34 -0300

    Show more data and hide

--- main.js
+++ main.js
@@ -1,40 +1,34 @@
-let data_info
 
-document.querySelector('.submit').addEventListener('click', (e)=>{
-    e.preventDefault();
-
-    let CITY_NAME = document.querySelector('#input').value;
-   
+document.querySelector('.submit').addEventListener('click', ()=>{
+    let data_info
+    
+    let CITY_NAME = document.querySelector('.input').value;
     getApi();
     if(!CITY_NAME) return
-    createElement();
 
-    console.log(data_info)
-
-
-    //criação dos elementos HTML
-
-
-
-    
 })
 
 function getApi(){
     let API_KEY = 'd7009a0ec2a75fbdc312380c2578e2f7';
-    let CITY_NAME = document.querySelector('#input').value;
-    if(!CITY_NAME) return console.error('Digite um nome de cidade válido!');;
+    let CITY_NAME = document.querySelector('.input').value;
+    let language = 'pt_br'
+    if(!CITY_NAME) return console.error('Digite um nome de cidade válido!');
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&lang=${language}&appid=${API_KEY}&units=metric`)
     .then(res => res.json())
     .then(data => data_info = data)
-    .catch(error => console.log(error));
-    return data_info, CITY_NAME
+    .then(data_info => {
+        createElement();
+        return data_info;
+    });
 }
 function createElement(){
+    console.log(data_info)
     let div_tempo = document.querySelector('.tempo');
     let div = document.createElement('div');
     div.setAttribute('class','cidade');
     div_tempo.appendChild(div);
+  
     //elementos da div
     let cityP = document.createElement('p');
     cityP.setAttribute('id', 'city-name');
@@ -48,6 +42,43 @@ function createElement(){
 
     let cityW = document.createElement('p');
     cityW.setAttribute('class', 'city-weather');
-    cityW.textContent = data_info.weather[0].main;
+    cityW.textContent = data_info.weather[0].description;
     div.appendChild(cityW);
+
+    let divMenosInfo = document.createElement('div');
+    divMenosInfo.setAttribute('class', 'menos-info');
+    div.appendChild(divMenosInfo);
+
+    let minTemp = document.createElement('p');
+    minTemp.textContent = `Min. Temp: ${data_info.main.temp_min}`;
+    divMenosInfo.appendChild(minTemp);
+    
+    let maxTemp = document.createElement('p');
+    maxTemp.textContent = `max temp: ${data_info.main.temp_max}`;
+    divMenosInfo.appendChild(maxTemp);
+
+    let umidade = document.createElement('p');
+    umidade.textContent = `úmidade: ${data_info.main.humidity}`;
+    divMenosInfo.appendChild(umidade);
+
+    let pressao = document.createElement('p');
+    pressao.textContent = `Pressão: ${data_info.main.pressure}`;
+    divMenosInfo.appendChild(pressao);
+
+    let divNormal = document.createElement('div');
+    divNormal.setAttribute('class', 'normal');
+    divNormal.setAttribute('id', 'expandir');
+    div.appendChild(divNormal);
+
+    document.getElementById('expandir').addEventListener('click', (e)=> {
+        document.querySelector('.cidade').classList.toggle('expandido');
+        document.querySelector('.menos-info').classList.toggle('extra-info');
+        document.querySelector('.normal').classList.toggle('expandir');
+    })
+
 }
+
+
+/*
+
+*/
